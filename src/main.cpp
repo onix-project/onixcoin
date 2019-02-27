afa1242aaa73116b4d3fbd5de21462ea7ec3e196
Heber Bastidas <heberbastidas@gmail.com>
2019-02-27 05:21:54 -0400

    EMERGENCY UPDATE: fix kimoto gravity well exploit https://bitcointalk.org/index.php?topic=552895.0

--- src/main.cpp
+++ src/main.cpp
@@ -1129,6 +1129,7 @@ unsigned int static KimotoGravityWell(const CBlockIndex* pindexLast, const CBloc
 
     if (BlockLastSolved == NULL || BlockLastSolved->nHeight == 0 || (uint64)BlockLastSolved->nHeight < PastBlocksMin) { return bnProofOfWorkLimit.GetCompact(); }
 
+  int64 LatestBlockTime = BlockLastSolved->GetBlockTime();
 	for (unsigned int i = 1; BlockReading && BlockReading->nHeight > 0; i++) {
 		if (PastBlocksMax > 0 && i > PastBlocksMax) { break; }
 		PastBlocksMass++;
@@ -1137,10 +1138,20 @@ unsigned int static KimotoGravityWell(const CBlockIndex* pindexLast, const CBloc
 		else		{ PastDifficultyAverage = ((CBigNum().SetCompact(BlockReading->nBits) - PastDifficultyAveragePrev) / i) + PastDifficultyAveragePrev; }
 		PastDifficultyAveragePrev = PastDifficultyAverage;
 
-		PastRateActualSeconds			= BlockLastSolved->GetBlockTime() - BlockReading->GetBlockTime();
+    if (LatestBlockTime < BlockReading->GetBlockTime()) {
+        if (BlockReading->nHeight > 317910) LatestBlockTime = BlockReading->GetBlockTime();
+    }
+    PastRateActualSeconds			= LatestBlockTime - BlockReading->GetBlockTime();
+    
 		PastRateTargetSeconds			= TargetBlocksSpacingSeconds * PastBlocksMass;
 		PastRateAdjustmentRatio			= double(1);
-		if (PastRateActualSeconds < 0) { PastRateActualSeconds = 0; }
+
+    if (BlockReading->nHeight > 317910) {
+        if (PastRateActualSeconds < 1) { PastRateActualSeconds = 1; }
+    } else {
+        if (PastRateActualSeconds < 0) { PastRateActualSeconds = 0; }
+    }
+
 		if (PastRateActualSeconds != 0 && PastRateTargetSeconds != 0) {
 		PastRateAdjustmentRatio			= double(PastRateTargetSeconds) / double(PastRateActualSeconds);
 		}
